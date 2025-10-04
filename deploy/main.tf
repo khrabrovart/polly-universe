47ecdd368e4b5653c6894a07e8e1770087525849
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-04 17:30:56 +0200

    Remove unnecessary resources

--- deploy/main.tf
+++ deploy/main.tf
@@ -9,11 +9,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "polly-universe-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "polly-universe-terraform-locks"
-    encrypt        = true
+    bucket  = "arturkhrabrov-tfstate"
+    key     = "polly-universe/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
@@ -25,56 +24,5 @@ provider "aws" {
       Project   = "PollyUniverse"
       ManagedBy = "Terraform"
     }
-  }
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "polly-universe-terraform-state"
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_encryption" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "polly-universe-terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform State Lock Table"
   }
 }
