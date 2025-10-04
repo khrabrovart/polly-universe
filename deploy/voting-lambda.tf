f7a49f977254f8ca5ad7880419b9f8fda1835055
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-04 22:20:13 +0200

    Try what copilot suggests

--- deploy/voting-lambda.tf
+++ deploy/voting-lambda.tf
@@ -8,8 +8,8 @@ resource "aws_lambda_function" "voting_lambda" {
   filename      = data.archive_file.voting_lambda_zip.output_path
   function_name = "${local.app_name}-voting"
   role          = aws_iam_role.voting_lambda_role.arn
-  handler       = "PollyUniverse.Voting.Func::PollyUniverse.Voting.Func.Function::HandleEvent"
-  runtime       = "dotnet8"
+  handler       = "bootstrap"
+  runtime       = "provided.al2"
   timeout       = 300
   memory_size   = 256
   architectures = ["arm64"]
