b4caf9356994fe5bbe94584956efa0b56da4eee3
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-04 17:33:30 +0200

    Remove unnecessary s3 resources

--- deploy/s3.tf
+++ deploy/s3.tf
@@ -8,24 +8,3 @@ resource "aws_s3_bucket_versioning" "polly_universe" {
     status = "Enabled"
   }
 }
-
-resource "aws_s3_bucket_encryption" "polly_universe" {
-  bucket = aws_s3_bucket.polly_universe.id
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "polly_universe" {
-  bucket = aws_s3_bucket.polly_universe.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
