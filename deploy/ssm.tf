490dc2f78686c2c2b52d253b2ae679fb2e16e34c
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-06 18:33:50 +0200

    Add ssm parameter values

--- deploy/ssm.tf
+++ deploy/ssm.tf
@@ -1,14 +1,29 @@
 resource "aws_ssm_parameter" "bot_token" {
-  name = "/polly-universe/bot-token"
-  type = "SecureString"
+  name  = "/polly-universe/bot-token"
+  type  = "SecureString"
+  value = ""
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "notifications_peer_id" {
-  name = "/polly-universe/notifications-peer-id"
-  type = "SecureString"
+  name  = "/polly-universe/notifications-peer-id"
+  type  = "SecureString"
+  value = ""
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "openai_api_key" {
-  name = "/polly-universe/openai-api-key"
-  type = "SecureString"
+  name  = "/polly-universe/openai-api-key"
+  type  = "SecureString"
+  value = ""
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
