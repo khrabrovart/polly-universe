3a061ddd5d074365eea4b826d40d5151f85fd1b8
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-06 18:41:49 +0200

    Add parameters placeholders

--- deploy/ssm.tf
+++ deploy/ssm.tf
@@ -1,17 +1,7 @@
 resource "aws_ssm_parameter" "bot_token" {
   name  = "/polly-universe/bot-token"
   type  = "SecureString"
-  value = ""
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "aws_ssm_parameter" "notifications_peer_id" {
-  name  = "/polly-universe/notifications-peer-id"
-  type  = "SecureString"
-  value = ""
+  value = "placeholder"
 
   lifecycle {
     ignore_changes = [value]
@@ -21,7 +11,17 @@ resource "aws_ssm_parameter" "notifications_peer_id" {
 resource "aws_ssm_parameter" "openai_api_key" {
   name  = "/polly-universe/openai-api-key"
   type  = "SecureString"
-  value = ""
+  value = "placeholder"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "notifications_peer_id" {
+  name  = "/polly-universe/notifications-peer-id"
+  type  = "String"
+  value = "placeholder"
 
   lifecycle {
     ignore_changes = [value]
