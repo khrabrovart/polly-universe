7ab34dced251c493261c7c3acd5c85fed425b0e2
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-09 18:02:45 +0200

    Add attributes

--- src/PollyUniverse/PollyUniverse.Func.Agent/Models/AgentRequest.cs
+++ src/PollyUniverse/PollyUniverse.Func.Agent/Models/AgentRequest.cs
@@ -1,11 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace PollyUniverse.Func.Agent.Models;
 
 public class AgentRequest
 {
+    [JsonPropertyName("message")]
     public AgentRequestMessage Message { get; set; }
 }
 
 public class AgentRequestMessage
 {
+    [JsonPropertyName("text")]
     public string Text { get; set; }
 }
