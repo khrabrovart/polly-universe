b4d702c3cf43c7378da118a4dda9de58707ad670
Artur Khrabrov <artur.khrabrov@moonpig.com>
2025-10-09 15:43:42 +0200

    Remove environment variables

--- src/PollyUniverse/PollyUniverse.Func.Agent/FunctionConfig.cs
+++ src/PollyUniverse/PollyUniverse.Func.Agent/FunctionConfig.cs
@@ -7,21 +7,8 @@ public class FunctionConfig
 {
     public FunctionConfig(IConfigurationRoot config)
     {
-        ScheduleNamePrefix = config.GetOrThrow("SCHEDULE_NAME_PREFIX");
-        SchedulerGroupName = config.GetOrThrow("SCHEDULER_GROUP_NAME");
-        TargetLambdaArn = config.GetOrThrow("TARGET_LAMBDA_ARN");
-        ScheduleExecutionRoleArn = config.GetOrThrow("SCHEDULE_EXECUTION_ROLE_ARN");
-
         IsDev = bool.TryParse(config["DEV:ENABLED"], out var isDev) && isDev;
     }
-
-    public string ScheduleNamePrefix { get; set; }
-
-    public string SchedulerGroupName { get; set; }
-
-    public string TargetLambdaArn { get; set; }
-
-    public string ScheduleExecutionRoleArn { get; set; }
 
     #region Development Settings
 
